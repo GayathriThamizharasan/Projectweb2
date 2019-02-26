25697f11898b609975887c04193845730f5e1189
ameyakadam <42661612+ameyakadam@users.noreply.github.com>
2019-02-26 13:21:28 -0700

    Initial commit

---
+++ ThamizharasanAddition/ThamizharasanAddition/Addition.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Addition.aspx.cs" Inherits="ThamizharasanAddition.Addition" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="Addition.css" />
+    
+    
+    <title></title>
+    <link rel="shortcut icon" type="png" href="Image/add1.png" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+            <div id ="body">
+            <h1>Custom Calculator To Calculate Sum of two numbers</h1>
+            </div>
+        
+          
+        <div id ="values">
+            <asp:Label ID="Label1" runat="server" Text="Please enter your name"></asp:Label>
+            <asp:TextBox ID="Name" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Enter first number"></asp:Label>
+            <asp:TextBox ID="Firstno" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Enter second number"></asp:Label>
+            <asp:TextBox ID="Secondno" runat="server"></asp:TextBox>
+            <br /><br />
+            &nbsp
+            <asp:Button ID="Button3" runat="server" Text="Add" OnClick="Button3_Click" />
+            <asp:Button ID="Button2" runat="server" Text="Clear" OnClick="Button2_Click" Width="73px" />
+            </div> <br />
+            <div id="answer">
+                <asp:Label ID="Label4" runat="server"></asp:Label>
+                </div>
+        
+    </form>
+</body>
+</html>
---
+++ ThamizharasanAddition/ThamizharasanAddition/Addition.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ThamizharasanAddition
+{
+    public partial class Addition : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+             string n = Name.Text;
+            int a = 0;
+            int b = 0;
+            try
+            {
+                int value1 = Convert.ToInt32(Firstno.Text);
+                int value2 = Convert.ToInt32(Secondno.Text);
+                if (value1 > 9999 | value2 > 9999)
+                {
+                    throw new OverflowException();
+                }
+                a = value1;
+                b = value2;
+            }
+            catch (FormatException)
+            {
+                Response.Write(@"<script language='javascript'>alert('Enter the value of a and b in range between 1 - 9999')</script>");
+                Firstno.Text = "";
+                Secondno.Text = "";
+            }
+            catch (OverflowException)
+            {
+                Response.Write(@"<script language='javascript'>alert('Enter the value of a and b in whole numbers less than 9999')</script>");
+                Firstno.Text = "";
+                Secondno.Text = "";
+            }
+            int c;
+            c = (a + b);
+            
+            Label4.Text = "Hello " + n + " your answer is: " + c;
+
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Name.Text = "";
+            Firstno.Text = "";
+            Secondno.Text = "";
+        }
+    }
+}
